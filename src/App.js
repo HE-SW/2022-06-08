eb9565a205098281093454dd61203292885b75c3
[SW2/서울/2반]김한얼 <cjswogksdjf@gmail.com>
2022-06-08 14:33:08 +0900

    0608

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href='/'
+          onClick={(evt) => {
+            console.log('evt', evt);
+            evt.preventDefault();
+            props.onSelect();
+          }}
+        >
+          WWW
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+function Nav(props) {
+  const liTags = props.data.map((e) => {
+    return (
+      <li key={e.id}>
+        <a
+          href={'/read/' + e.id}
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onSelect(e.title);
+          }}
+        >
+          {e.title}
+        </a>
+      </li>
+    );
+  });
+  return (
+    <nav>
+      <ol>{liTags}</ol>
+    </nav>
+  );
+}
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const topics = [
+    { id: 1, title: 'html', body: 'html is...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+  ];
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title='Welcome' body='Hello, WEB!'></Article>;
+  } else if (mode === 'READ') {
+    content = <Article title='READ' body='Hello, READ!'></Article>;
+  }
+  function createHandler() {
+    alert('create');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+        onSelect={() => {
+          // mode = 'WELCOME';
+          setMode('WELCOME');
+        }}
+      ></Header>
+      <Nav
+        data={topics}
+        onSelect={(id) => {
+          // mode = 'READ';
+          setMode('READ');
+        }}
+      ></Nav>
+      {content}
+      <ButtonGroup
+        variant='outlined'
+        aria-label='outlined primary button group'
+      >
+        <Button variant='outlined' onClick={createHandler}>
+          Create
+        </Button>
+        <Button variant='outlined'>Update</Button>
+      </ButtonGroup>
+      <Button variant='outlined'>delete</Button>
     </div>
   );
 }
